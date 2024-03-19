5cc9846cef6a1af108f2fd28f824e946ae3d19c0
tribeti <nguyenductri28072004@gmail.com>
2024-03-19 16:28:07 +0700

    Create bai4.cpp

---
+++ LAB7/bai4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[100];
+
+void print_partitions(int index, int value, int prev) {
+    if (value == 0) {
+        // In ra phân tích số
+        for (int i = 0; i < index; i++) {
+            cout << arr[i];
+            if (i != index - 1) {
+                cout << " + ";
+            }
+        }
+        cout << endl;
+        return;
+    }
+
+    // Thử các giá trị từ prev đến value
+    for (int i = prev; i <= value; i++) {
+        arr[index] = i;
+        print_partitions(index + 1, value - i, i);
+    }
+}
+
+int main() {
+    cout << "Nhap n (n <= 30): ";
+    cin >> n;
+
+    print_partitions(0, n, 1);
+
+    return 0;
+}
